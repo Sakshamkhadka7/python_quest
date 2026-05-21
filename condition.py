a1e12109d7c155e3622e5aaf180e0dd0744bd28f
Sakshamkhadka7 <sakshamcode12@gmail.com>
2026-05-21 08:03:38 +0545

    made a student file using datatypes

--- condition.py
+++ condition.py
@@ -3,5 +3,3 @@ value=input("Enter a number")
 if value.isdigit():
 num=int(value)
 print("Number is converted to float", float(num))
-
-
---
+++ student.py
@@ -0,0 +1,11 @@
+name="saksham khadka"
+roll=12
+address="birgunj"
+isStudent=True
+gpa=3.52
+
+print(name)
+print(roll)
+print(address)
+print(isStudent)
+print(gpa)
