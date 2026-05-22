f676e3d04ca75275449da576608bae82f22a4042
Sakshamkhadka7 <sakshamcode12@gmail.com>
2026-05-22 07:22:14 +0545

    Learn a list

---
+++ list.py
@@ -0,0 +1,17 @@
+numbers=[10,20,30,40,50]
+
+print(numbers[0])
+print(numbers[1])
+print(numbers[2])
+print(numbers[4])
+
+
+print(numbers[-1])
+print(numbers[-2])
+
+student = ["saksham", "kuldeep", "milan","saroj","adarsha"]
+
+for item in student:
+    print(item)  
+
+print(student[2:]) 
